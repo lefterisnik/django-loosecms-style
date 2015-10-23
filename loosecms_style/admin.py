54040d6205a3718ee34c63b96bc6cbd217bb2411
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-10-23 13:49:48 +0300

    Add filter to class admin.

--- loosecms_style/admin.py
+++ loosecms_style/admin.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from django import forms
+from django.db import models
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Style, StyleClass, StyleClassAttribute
 
@@ -12,12 +15,20 @@ class StyleAdmin(admin.ModelAdmin):
 class StyleClassAttributeInline(admin.StackedInline):
     model = StyleClassAttribute
     extra = 1
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'rows': 5})},
+    }
 
 
 class StyleClassAdmin(admin.ModelAdmin):
     inlines = [
         StyleClassAttributeInline
     ]
+    search_fields = ['title']
+    list_filter = ('styleclassattribute__override', )
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'rows': 5})},
+    }
 
 admin.site.register(Style, StyleAdmin)
 admin.site.register(StyleClass, StyleClassAdmin)
